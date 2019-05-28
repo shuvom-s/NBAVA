329ec32666c26ab92c4fc5a513162949d7407052
Shuvom Sadhuka <ssadhuka@college.harvard.edu>
2019-05-27 19:54:05 -0700

    Update lottery-simulation.R

--- lottery-simulation.R
+++ lottery-simulation.R
@@ -8,8 +8,23 @@ library(rvest)
 library(RSelenium)
 library(RCurl)
 library(jsonlite)
-setwd("/Users/shuvomsadhuka/Documents/GitHub/NBAVA")
+library(NLP)
+library(naniar)
+library(rapport)
+library(sjmisc)
+library(ggplot2)
+library(reticulate)
+library(RhpcBLASctl)
+##import all libraries necessary for this script -- it's a lot!
 
+# Setup parallel computation - use all cores on our computer.
+num_cores = RhpcBLASctl::get_num_cores()
+num_cores
+options(mc.cores = num_cores)
+getOption("mc.cores")
+
+
+setwd("/Users/shuvomsadhuka/Documents/GitHub/NBAVA")
 csvreader <- function(x)
 {
   nba <- read.csv(x)
@@ -20,41 +35,142 @@ csvreader <- function(x)
   
   as.matrix <- matrix(ncol = n)
   for (i in 1:61){
-    as.strings[i] <- toString(nba[i,])
+    #as.strings[i] <- toString(nba[i,])
     total <-  as.vector(unlist(strsplit(as.character(nba[i,]), ",")))
+    
+    total[total==""]  <- NA 
     as.matrix <- rbind(as.matrix, total)
   }
   as.matrix <- as.matrix[-1,] #delete the first row of the table -- kind of buggy
   colnames(as.matrix) <- as.character(unlist(as.matrix[1,]))
+  year <- substr(x, 1, 4)
   as.matrix <- as.matrix[-1,] #delete the row we just made the header row
-  return(as.matrix)
+  df <- data.frame(as.matrix)
+  df$year <- rep(year, length(df[,1])) #store the year of the draft in a separate column
+  df_lottery <- df[1:14, ]
+  return(df_lottery)
 }
 
 fileList <- list.files(path="/Users/shuvomsadhuka/Documents/GitHub/NBAVA", pattern=".csv")
-
+all_drafts <- matrix(ncol = 22)
+df_bind <- data.frame()
 for(file in fileList){
-  csvreader(file)
+  df <- csvreader(file)
+  df_bind <- rbind(df_bind, df)
+}
+rownames(df_bind) <- c()
+df_bind
+typeof(df_bind$Rk)
+
+new <- df_bind[df_bind$Rk == 4,]
+vorp.col = grep("VORP", colnames(new)); bpm.col = grep("BPM", colnames(new));
+ws.col = grep("WS.48", colnames(new))
+cols <- c(vorp.col, bpm.col, ws.col)
+
+length(new$VORP)
+
+new[cols] <- unname(new[cols])
+new$VORP <- as.numeric(as.character(new$VORP))
+new$WS.48 <- as.numeric(as.character(new$WS.48))
+new$BPM <- as.numeric(as.character(new$BPM))
+
+avg = mean(new$VORP)
+var = var(new$VORP)
+
+alpha = avg^2/var
+beta = avg/var
+mean(new$BPM)
+
+
+
+#colnames(mydata) <- paste0("position_", 1:3)
+scalar1 <- function(x) {x / sqrt(sum(x^2))}
+
+
+draft_position <- function(x){
+  pos <- df_bind[df_bind$Rk == x,]
+  vorp.col = grep("VORP", colnames(pos)); bpm.col = grep("BPM", colnames(pos)); 
+  ws.col = grep("WS.48", colnames(pos))
+  cols <- c(vorp.col, bpm.col, ws.col)
+  
+  pos[cols] <- unname(pos[cols])
+  pos$VORP <- as.numeric(as.character(pos$VORP))
+  pos$WS.48 <- as.numeric(as.character(pos$WS.48))
+  pos$BPM <- as.numeric(as.character(pos$BPM))
+  
+  avg_vorp = mean(pos$VORP, na.rm=TRUE); avg_ws = mean(pos$WS.48, na.rm=TRUE); avg_bpm = mean(pos$BPM, na.rm=TRUE)
+  var_vorp = var(pos$VORP, na.rm=TRUE); var_ws = var(pos$WS.48, na.rm=TRUE); var_bpm = var(pos$BPM, na.rm=TRUE)
+  
+  alpha_vorp = avg_vorp^2/var_vorp; alpha_ws = avg_ws^2/var_ws; alpha_bpm = avg_bpm^2/var_bpm; 
+  beta_vorp = avg/var; beta_ws =avg_ws/var_ws; beta_bpm = avg_bpm/var_bpm; 
+  
+  pos_vector <- c(avg_vorp, var_vorp, alpha_vorp, beta_vorp,
+                  avg_ws, var_ws, alpha_ws, beta_ws,
+                  avg_bpm, var_bpm, alpha_bpm, beta_bpm)
+  
+  return(pos_vector)
+}
+positions <- c(1:14)
+parameters <- sapply(positions, draft_position)
+rownames(parameters) <- c("avg_vorp", "var_vorp", "alpha_vorp", "beta_vorp",
+                          "avg_ws", "var_ws", "alpha_ws", "beta_ws",
+                          "avg_bpm", "var_bpm", "alpha_bpm", "beta_bpm")
+parameters
+
+
+lottery_teams <- cbind(c(14,14,14,12.5,10.5,9,6,6,6,3,2,1,1,1), 
+                       c(13.4,13.4,13.4,12.2,10.5,9.2,6.3,6.3,6.3,3.3,2.2,1.1,1.1,1.1),
+                       c(12.7,12.7,12.7,11.9,10.5,9.4,6.7,6.7,6.7,3.6,2.4,1.2,1.2,1.2),
+                       c(11.9,11.9,11.9,11.4,10.5,9.6,7.2,7.2,7.2,4.0,2.8,1.4,1.4,1.4),
+                       c(47.9,27.8,14.8,7.2,2.2,0,0,0,0,0,0,0,0,0),
+                       c(0,20.1,26.0,25.7,19.6,8.6,0,0,0,0,0,0,0,0),
+                       c(0,0,7.1,16.8,26.7,29.6,19.7,0,0,0,0,0,0,0),
+                       c(0,0,0,2.2,8.8,20.6,37.2,31.2,0,0,0,0,0,0),
+                       c(0,0,0,0,0.6,3.8,15.1,34.1,46.4,0,0,0,0,0),
+                       c(0,0,0,0,0,0.2,1.6,8.0,24.3,65.9,0,0,0,0),
+                       c(0,0,0,0,0,0,0.1,0.5,2.9,18.9,77.6,0,0,0),
+                       c(0,0,0,0,0,0,0,0.1,0.1,1.2,12.6,86.1,0,0),
+                       c(0,0,0,0,0,0,0,0,0.1,0.1,0.4,9,90.6,0),
+                       c(0,0,0,0,0,0,0,0,0,0.1,0.1,0.2,4.6,95.2))
+
+lottery_teams <- lottery_teams/100
+
+teams <- c(1:14)
+
+simulate <- function(){
+  picks <- c()
+  for(i in 1:14) {
+    if (i == 1){
+      pick.i <- sample(teams,1,replace = FALSE, prob = scalar1(lottery_teams[,1]))
+    }
+    else if(i < 4){
+      pick.i <- sample(teams[-picks],1,replace = FALSE, prob = scalar1(lottery_teams[,i][-picks])) 
+    }
+    else{
+     remaining <- teams[-picks]
+     pick.i <- remaining[1]
+    }
+    
+    picks <- c(picks, pick.i)
+  }
+  return(picks)
 }
 
-View(csvreader('2011-Draft.csv'))
+drafts <- replicate(10000, simulate()) #each column is a simulation of a draft
 
+mean_var_pos <- function(x){
+  positions <- vector()
+  for(i in 1:10000){
+    pos <- which(drafts[,i] == x)
+    positions <- c(positions, pos)
+  }
+  return(c(mean(positions), var(positions)))
+}
 
+for(i in 1:14){
+  print(mean_var_pos(i))
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-total
-nba_2018[9,]
 shell('docker run -d -p 4445:4444 selenium/standalone-firefox')
 # lebron <- "https://www.basketball-reference.com/players/j/jamesle01.html"
 # rD <- rsDriver()
